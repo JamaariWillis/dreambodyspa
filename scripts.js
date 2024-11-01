82f5729bc17c9d62fc32063e19e8b6d8888539b9
Jamaari <jamaari1w@gmail.com>
2024-11-01 05:40:08 -0400

    Booking update

--- scripts.js
+++ scripts.js
@@ -10,3 +10,25 @@
         }
     };
 
+    (function (a) {
+        var b = {
+          businessId: '108503ee-d9f4-48f8-aaaa-69cb7ec3865b',
+        };
+      
+        var c = a.createElement('script');
+        var d = a.querySelector('script');
+      
+        c.src = 'https://static.joinboulevard.com/injector.min.js';
+        c.async = true;
+        c.onload = function () {
+          blvd.init(b);
+      
+          // Add the event listener for the "Book Now" button once the script is loaded
+          document.getElementById('book-now-button').addEventListener('click', function() {
+            blvd.open();
+          });
+        };
+      
+        d.parentNode.insertBefore(c, d);
+      })(document);
+      
